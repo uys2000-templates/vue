563029e817f5f3b6f8810253afee2f68dc784dbc
Mehmet Uysal <siliente2020@gmail.com>
2025-02-18 19:52:34 +0300

    reinit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <router-view v-slot="{ Component }">
+    <transition name="layout">
+      <component :is="Component" />
+    </transition>
+  </router-view>
+</template>
+
+<script lang="ts">
+import { RouterView } from 'vue-router'
+export default {
+  components: {
+    RouterView
+  }
+}
+</script>
+
+
+<style>
+.layout-enter-active,
+.layout-leave-active {
+  transition: opacity 0.5s ease;
+  position: absolute;
+}
+
+.layout-enter-from,
+.layout-leave-to {
+  opacity: 0;
+}
+</style>
---
+++ src/components/shared/TheHeader.vue
@@ -0,0 +1,50 @@
+<template>
+  <header class="px-4 pt-4 sticky top-0 z-10">
+    <div class="bg-neutral text-neutral-content flex justify-between items-center p-4 rounded-box">
+      <RouterLink class="btn btn-sm btn-ghost" :to="{ name: 'HomeView' }">
+        Mehmet Uysal
+      </RouterLink>
+      <div class="flex gap-2">
+        <RouterLink class="btn btn-sm btn-ghost" :to="{ name: 'AboutView' }">
+          About
+        </RouterLink>
+        <Dropdown class="dropdown-end">
+          <template #title>
+            <div class="btn btn-sm btn-ghost">
+              Menu
+            </div>
+          </template>
+          <template #content>
+            <DropdownContent class="bg-neutral rounded-btn w-max mt-1">
+              <div class="flex flex-col">
+                <RouterLink class="btn btn-sm btn-ghost" :to="{ name: 'AppView' }">
+                  App
+                </RouterLink>
+              </div>
+            </DropdownContent>
+          </template>
+        </Dropdown>
+        <ThemeBtn />
+      </div>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import { RouterLink } from 'vue-router';
+import Dropdown from '../daisy/Dropdown.vue';
+import DropdownContent from '../daisy/DropdownContent.vue';
+import ThemeBtn from '../daisy/ThemeBtn.vue';
+import { useAuthStore } from '@/stores/auth';
+
+export default {
+  components: { RouterLink, Dropdown, DropdownContent, ThemeBtn },
+  data() {
+    return {
+      authStore: useAuthStore()
+    }
+  },
+  methods: {
+  }
+}
+</script>
